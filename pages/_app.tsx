474c204b54f8ee46ad9807762b71d5272e38cf35
DharuvManchanda <dharuvmanchanda001@gmail.com>
2025-02-28 01:16:41 +0530

    gsv-added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Best Makeup Artist - Anjali Bhutani | Bridal & Party Makeup</title>
+        <meta name="google-site-verification" content="Zvu-78XubYkeGnVEN-4iaFlCWQvq-80WTF0OcD3D5Fg" />
         <meta name="description" content="Anjali Bhutani is a professional makeup artist specializing in bridal, party, and HD makeup. Book your session today and look stunning!" />
         <meta name="keywords" content="makeup artist, bridal makeup, party makeup, HD makeup, best makeup artist, professional makeup, wedding makeup, airbrush makeup, makeup studio, makeup services" />
         <meta name="author" content="Anjali Bhutani" />
